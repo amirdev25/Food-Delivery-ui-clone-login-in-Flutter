c61742e445ea65e85825c6d6a2c12a2f97076f62
Og'abek <amirdev25@gmail.com>
2023-01-21 20:46:10 +0500

    Date:21.01.2023

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -21,85 +22,153 @@ class LoginBody extends StatefulWidget {
 class _LoginBodyState extends State<LoginBody> {
   String? mLogin = "";
   String mPassword = "";
+  bool isLoginTrue = false;
+  bool isPasswordTrue = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/login_bg.png"),
-          fit: BoxFit.fill,
-        ),
-      ),
-      alignment: Alignment.center,
+    return SingleChildScrollView(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.blueAccent,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                labelText: 'Username',
+          Container(
+            height: 200.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/img.png"),
+                fit: BoxFit.fill,
               ),
-              onSaved: (String? value) {
-                mLogin = value;
-              },
-              validator: (String? value) {
-                return (value != null && value.contains('@'))
-                    ? 'Do not use the @ char.'
-                    : null;
-              },
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8.0),
-                  ),
-                ),
-                labelText: "Password",
-              ),
-              cursorColor: Colors.blueAccent,
-            ),
-          ),
-          const Text(
-            "Forgot password?",
-            style: TextStyle(color: Colors.grey),
-          ),
-          InkWell(
             child: Container(
-              margin: const EdgeInsets.all(16.0),
-              width: double.infinity,
               alignment: Alignment.center,
-              height: 48.0,
-              decoration: const BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(24.0),
-                ),
-              ),
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                    width: 56.0,
+                    height: 56.0,
+                    image: AssetImage("assets/images/img_burger_coffe.png"),
+                  ),
+                  const SizedBox(
+                    width: 4.0,
+                  ),
+                  SizedBox(
+                    height: 56.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Container(
+                          height: 20.0,
+                          width: 120.0,
+                          alignment: Alignment.center,
+                          child: SvgPicture.asset(
+                            "assets/images/name_text.svg",
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          )
+          ),
+          const SizedBox(
+            height: 100.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  // height: 48.0,
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    expands: false,
+                    maxLines: 100,
+                    minLines: 1,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 0.0,
+                        horizontal: 5.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.blueAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      labelText: 'Username',
+                    ),
+                    onSaved: (String? value) {
+                      mLogin = value;
+                    },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 0.0,
+                        horizontal: 5.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.blueAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      labelText: 'Password',
+                    ),
+                    onSaved: (String? value) {
+                      mLogin = value;
+                    },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                ),
+              ),
+              const Text(
+                "Forgot password?",
+                style: TextStyle(color: Colors.grey),
+              ),
+              InkWell(
+                child: Container(
+                  margin: const EdgeInsets.all(16.0),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  height: 48.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(24.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
